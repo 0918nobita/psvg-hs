5c72c810259e01c31b92d054379e79ad1a9e73e3
0918nobita <nobita.0918@gmail.com>
2020-11-29 22:33:19 +0900

    :construction: Parser, ParserContext の定義を修正

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE InstanceSigs        #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Lib
@@ -7,60 +6,73 @@ module Lib
 
 import qualified Data.Dynamic as DYN
 import qualified Data.Map     as M
--- import Debug.Trace (trace)
+import           Debug.Trace  (trace)
 
 type Start = Int
 
 type ParserId = Int
 
 data ParserContext = ParserContext
-    { source :: String
-    , memory :: M.Map (Start, ParserId) (DYN.Dynamic, String)
+    { source   :: String
+    , position :: Int
+    , memory   :: M.Map (Start, ParserId) (DYN.Dynamic, Int)
     }
     deriving (Show)
 
 data Parser e a = Parser
     { parserId :: Maybe ParserId
-    , parseFn  :: String -> Either e (a, String)
+    , parseFn  :: ParserContext -> Either e (a, ParserContext)
     }
-
-instance Functor (Parser e) where
-    fmap :: forall a b . (a -> b) -> Parser e a -> Parser e b
-    fmap f p = Parser { parserId = Nothing, parseFn = parse }
-        where
-            parse :: String -> Either e (b, String)
-            parse src = parseFn p src >>= (\(a, rest) -> Right (f a, rest))
-
-runParser :: DYN.Typeable a => Parser e a -> ParserContext -> Start -> Either e ((a, String), ParserContext)
-runParser p context start =
-    let
-        memo = memory context
-        src = source context
-    in
-    case parserId p of
-        Just pId ->
-            case M.lookup (start, pId) memo >>= (\(a, rest) -> DYN.fromDynamic a >>= (\dyn -> Just (dyn, rest))) of
-                Just success -> Right (success, context)
-                Nothing -> do
-                    (a, rest) <- parseFn p $ drop start src
-                    let newMemo = M.insert (start, pId) (DYN.toDyn a, rest) memo
-                    return ((a, rest), ParserContext { source = src, memory = newMemo })
-        Nothing -> do
-            success <- parseFn p $ drop start src
-            return (success, context)
 
 char :: Char -> Parser () Char
 char c = Parser { parserId = Just 1, parseFn = parse }
     where
-        parse :: String -> Either () (Char, String)
-        parse (head:tail)
-            | head == c = Right (c, tail)
-            | otherwise = Left ()
-        parse _ = Left ()
+        parse :: ParserContext -> Either () (Char, ParserContext)
+        parse context =
+            let
+                src = source context
+                pos = position context
+            in
+            case drop pos src of
+                head:_ | head == c ->
+                    Right (c, ParserContext { source = src, position = pos + 1, memory = memory context })
+                _ -> Left ()
+
+mapParser :: forall a b e . DYN.Typeable a => (a -> b) -> Parser e a -> Parser e b
+mapParser f p = Parser { parserId = Nothing, parseFn = parse }
+    where
+        parse :: ParserContext -> Either e (b, ParserContext)
+        parse context = do
+            (a, context) <- runParser p context
+            return (f a, context)
+
+runParser :: DYN.Typeable a => Parser e a -> ParserContext -> Either e (a, ParserContext)
+runParser p context =
+    let
+        memo = memory context
+        src = source context
+        start = position context
+    in
+    case parserId p of
+        Just pId ->
+            let
+                checkCache = do
+                    (a, rest) <- M.lookup (start, pId) memo
+                    dyn <- DYN.fromDynamic a
+                    return (dyn, rest)
+            in
+            case trace "Checking cache..." checkCache of
+                Just (a, tailPos) -> trace "Cache found" Right (a, ParserContext { source = src, position = tailPos, memory = memo })
+                Nothing -> do
+                    (a, context) <- trace "Parsing..." parseFn p context
+                    let pos = position context
+                    let newMemo = M.insert (start, pId) (DYN.toDyn a, pos) memo
+                    return (a, ParserContext { source = src, position = pos, memory = newMemo })
+        Nothing -> parseFn p context
 
 someFunc :: IO ()
 someFunc = do
-    case runParser (char 'a') (ParserContext { source = "abc", memory = M.empty }) 0 of
-        Right (_, context) ->
-            print $ runParser ((: "!") <$> char 'a') context 0
-        Left _ -> putStrLn "Left"
+    let context = ParserContext { source = "abc", position = 0, memory = M.empty }
+    case runParser (mapParser (: "!") (char 'a')) context of
+        Right r -> print r
+        Left _  -> putStrLn "Left"
