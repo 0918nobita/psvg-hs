0c0be7f6a46c74cecc231a86f6324ca0c1b6040e
0918nobita <nobita.0918@gmail.com>
2020-11-30 15:20:36 +0900

    動かない

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,34 +1,74 @@
+{-# LANGUAGE InstanceSigs        #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Lib
     ( someFunc
     ) where
 
-import           Control.Monad.ST (runST)
-import qualified Data.Map.Strict  as M
-import           Data.STRef       (modifySTRef, newSTRef, readSTRef)
+import           Control.Monad.ST (ST, runST)
+import qualified Data.Bifunctor   as B
+import qualified Data.Map         as M
+import           Data.STRef       (STRef, modifySTRef, newSTRef, readSTRef)
 import           Debug.Trace      (trace)
+import           Text.Printf      (printf)
 
 someFunc :: IO ()
 someFunc = do
-    print $ char 'a' "abc"
-    print $ char 'b' "bxx"
-    print $ char 'a' "abc"
+    let p = char 'a'
+    print $ parseFn p "abc"
+    print $ parseFn (char 'b') "bxx"
+    print $ parseFn (p >> char 'b') "abc"
 
-type Parser e a = String -> Either e (a, String)
+type Memory s a = ST s (STRef s (M.Map String (a, String)))
 
-char :: Char -> Parser () Char
-char c s =
-    trace "Parsing..." $
-        case s of
-            (head:tail) | head == c -> Right (c, tail)
-            _                       -> Left ()
+type ParseFn e a = String -> Either e (a, String)
 
-memorize :: Ord a => (a -> b) -> a -> b
-memorize f =
-    let mapRef = newSTRef M.empty in
-    \a ->
-        case M.lookup a $ runST (mapRef >>= readSTRef) of
-            Just b  -> b
-            Nothing ->
-                let b = f a in
-                let _ = runST (mapRef >>= (\m -> modifySTRef m (M.insert a b))) in
-                b
+data Parser e s a = Parser
+    { memory  :: Memory s a
+    , parseFn :: ParseFn e a
+    }
+
+instance Functor (Parser e s) where
+    fmap f p = Parser { memory = newSTRef M.empty, parseFn = fmap (B.first f) . parseFn p }
+
+instance Applicative (Parser e s) where
+    pure a = Parser { memory = newSTRef M.empty, parseFn = \s -> return (a, s) }
+
+    (<*>) :: forall a b . Parser e s (a -> b) -> Parser e s a -> Parser e s b
+    pf <*> pa = Parser { memory = newSTRef M.empty, parseFn = parse }
+        where
+            parse :: String -> Either e (b, String)
+            parse s = do
+                (f, rest) <- parseFn pf s
+                (a, rest') <- parseFn pa rest
+                return (f a, rest')
+
+instance Monad (Parser e s) where
+    (>>=) :: forall a b . Parser e s a -> (a -> Parser e s b) -> Parser e s b
+    p >>= k = Parser { memory = newSTRef M.empty, parseFn = parse }
+        where
+            parse :: String -> Either e (b, String)
+            parse s = do
+                (a, rest) <- parseFn p s
+                parseFn (k a) rest
+
+char :: Char -> Parser () s Char
+char c =
+    let
+        memory = newSTRef M.empty
+    in
+    Parser { memory = memory, parseFn = memorize memory parse }
+    where
+        parse s = trace (printf "Parsing... %c" c) $
+            case s of
+                (head:tail) | head == c -> Right (c, tail)
+                _                       -> Left ()
+
+memorize :: Ord a => Memory s a -> ParseFn e a -> ParseFn e a
+memorize memo f src =
+    case M.lookup src $ runST (memo >>= readSTRef) of
+        Just a  -> Right a
+        Nothing -> do
+            r <- f src
+            let _ = runST (memo >>= (\m -> modifySTRef m (M.insert src r)))
+            return r
